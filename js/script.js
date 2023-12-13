ece46add4c95f33c5ef57adf2cc9cc4ef960fdcf
tiubarore <tiubarore93@gmail.com>
2023-12-13 12:56:11 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Replace text in header
+
+const checkReplace = document.querySelector(".replace-me");
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: "animated fadeIn",
+    speed: 1000,
+    separator: ",",
+    loopCount: "infinite",
+    autoRun: true,
+  });
+}
+
+// User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("border-bottom");
+      navbar.classList.add("border-secondary");
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("border-bottom");
+      navbar.classList.remove("border-secondary");
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
